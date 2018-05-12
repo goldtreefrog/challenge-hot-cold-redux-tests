58f9de0242978d063934e7ed6e166acda7b84371
Margaret Blauvelt <margaret@blauvelt.com>
2018-05-12 11:27:00 -0700

    Use snapshot testing: Add snapshot test to feedback.test.js and add enzyme-to-json to package.json

--- src/tests/feedback.test.js
+++ src/tests/feedback.test.js
@@ -3,14 +3,15 @@ import { shallow } from "enzyme";
 import { Feedback } from "../components/feedback";
 
 describe("<Feedback />", () => {
-  it("Renders without crashing", () => {
-    shallow(<Feedback />);
+  it("Renders without crashing and matches snapshot", () => {
+    const wrapper = shallow(<Feedback />);
+    expect(wrapper).toMatchSnapshot();
   });
-});
 
-it("Renders feedback", () => {
-  let TEST_FEEDBACK = "You are listening to the Hot or Cold game!";
+  it("Renders feedback", () => {
+    let TEST_FEEDBACK = "You are listening to the Hot or Cold game!";
 
-  let wrapper = shallow(<Feedback feedback={TEST_FEEDBACK} />);
-  expect(wrapper.contains(TEST_FEEDBACK)).toEqual(true);
+    let wrapper = shallow(<Feedback feedback={TEST_FEEDBACK} />);
+    expect(wrapper.contains(TEST_FEEDBACK)).toEqual(true);
+  });
 });
